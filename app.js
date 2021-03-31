68601a71ecf0aaf6db5800501b216dc2a5bcd44c
vinod540 <77802816+vinod540@users.noreply.github.com>
2021-03-31 13:53:21 -0500

    endpoints are working good in slot page

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var phoneRouter = require('./routes/phone');
 var starsRouter = require('./routes/stars');
+var slotRouter = require('./routes/slot');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/phone', phoneRouter);
 app.use('/stars', starsRouter);
+app.use('/slot', slotRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
